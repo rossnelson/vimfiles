a38076ded0a62ffec2eedebe9129e8059359a086
Ross Nelson <axcess1@me.com>
2023-12-25 22:35:51 -0500

    Clean up lua line

--- nvim/lua/plugins/line.lua
+++ nvim/lua/plugins/line.lua
@@ -1,36 +1,10 @@
 return {
   {
     "nvim-lualine/lualine.nvim",
-    config = function()
-      require('lualine').setup {
-        options = {
-          theme = "nord",
-          component_separators = '|',
-          section_separators = { left = '', right = '' },
-        },
-        sections = {
-          lualine_a = {
-            { 'mode', separator = { left = '' }, right_padding = 0 },
-          },
-          lualine_b = { 'filename', 'branch' },
-          lualine_c = { 'fileformat' },
-          lualine_x = {},
-          lualine_y = { 'filetype', 'progress' },
-          lualine_z = {
-            { 'location', separator = { right = '' }, left_padding = 0 },
-          },
-        },
-        inactive_sections = {
-          lualine_a = { 'filename' },
-          lualine_b = {},
-          lualine_c = {},
-          lualine_x = {},
-          lualine_y = {},
-          lualine_z = { 'location' },
-        },
-        tabline = {},
-        extensions = {},
-      }
+    config = function(opts)
+      opts.theme = "nord"
+
+      require('lualine').setup(opts)
     end,
   }
 }
