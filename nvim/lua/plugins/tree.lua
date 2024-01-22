ab404d5d4420b55ae3aaa8c2140e3fcbcb35eebc
Ross Nelson <ross.nelson@reguardprotection.com>
2024-01-22 13:42:09 -0500

    add shortcuts

--- nvim/lua/plugins/tree.lua
+++ nvim/lua/plugins/tree.lua
@@ -9,6 +9,7 @@ return {
   {
     'stevearc/oil.nvim',
     opts = {
+      skip_confirm_for_simple_edits = true,
       columns = {
         "icon",
       },
@@ -33,48 +34,19 @@ return {
     },
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
-  }
 
-  --
-  -- {
-  --   "nvim-neo-tree/neo-tree.nvim",
-  --   branch = "v3.x",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-  --     "MunifTanjim/nui.nvim",
-  --     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  --   },
-  --   config = function (opts)
-  --     local config = require "config.tree"
-  --     require("neo-tree").setup(config)
-  --
-  --     local map = vim.api.nvim_set_keymap
-  --     local opts = { noremap = true, silent = true }
-  --
-  --     map("n", "-", ":Neotree filesystem reveal float<CR>", opts)
-  --
-  --     -- --
-  --     -- Dont Need these anymore
-  --     -- --
-  --     -- map("n", "<leader>v", ":vsplit | Neotree<CR>", opts)
-  --     -- map("n", "<leader>s", ":split | Neotree<CR>", opts)
-  --
-  --     -- -- 
-  --     -- Cant get this to work, not sure why
-  --     -- https://github.com/nvim-neo-tree/neo-tree.nvim/blob/main/lua/neo-tree/ui/highlights.lua#L23
-  --     -- --
-  --     --
-  --     -- vim.api.nvim_set_hl(0, 'NormalFloat', { guibg = "#1e222a" })
-  --     -- vim.api.nvim_set_hl(0, 'FloatBorder', { bg = "#aa5a64", fg = "#aa5a64" })
-  --     -- vim.api.nvim_set_hl(0, 'NeoTreeNormalNC', { bg = "#aa5a64", fg = "#aa5a64" })
-  --     -- vim.api.nvim_set_hl(0, 'NeoTreeNormal', { bg = "#aa5a64", fg = "#aa5a64" })
-  --     -- vim.api.nvim_set_hl(0, 'NeoTreeFloatBorder', { bg = "#aa5a64", fg = "#aa5a64" })
-  --     -- vim.api.nvim_set_hl(0, 'NeoTreeFloatNormal', { bg = "#aa5a64", fg = "#aa5a64" })
-  --     -- vim.api.nvim_set_hl(0, 'NeoTreeFloatTitle', { bg = "#aa5a64", fg = "#aa5a64" })
-  --     -- vim.api.nvim_set_hl(0, 'NeoTreeFloatBorder', { guifg = "#aa5a64" })
-  --     -- vim.api.nvim_set_hl(0, 'NeoTreeFloatTitle', { guifg = "#aa5a64" })
-  --
-  --   end,
-  -- },
+    config = function()
+      require("oil").setup()
+
+      vim.keymap.set("n", "<leader>v", function()
+        vim.cmd("vsplit | wincmd l")
+        require("oil").open()
+      end)
+
+      vim.keymap.set("n", "<leader>s", function()
+        vim.cmd("split | wincmd j")
+        require("oil").open()
+      end)
+    end,
+  }
 }
