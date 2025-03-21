cc8df360e3bbee353ea91293bce31d1f33550a95
Ross Nelson <axcess1@me.com>
2025-03-21 16:11:35 -0400

    feat(nvim): update plugins and autocmd settings
    - Updated lazy-lock.json with new plugin commits
    - Changed FileType autocmd pattern to 'svelte'
    - Refactored gitsigns.nvim configuration

--- nvim/lua/core/autocmd.lua
+++ nvim/lua/core/autocmd.lua
@@ -14,7 +14,7 @@ local augroup = vim.api.nvim_create_augroup   -- Create/get autocommand group
 local autocmd = vim.api.nvim_create_autocmd   -- Create autocommand
 
 autocmd('FileType', {
-  pattern = '*',
+  pattern = 'svelte',
   command = 'setlocal noexpandtab listchars=tab:  ,trail:.,extends:#,nbsp:.'
 })
 
